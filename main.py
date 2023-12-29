6f1d6db6df7bf9f4eb7902e74d370687981bc06b
Vanda10 <vandabest123@gmail.com>
2023-12-29 13:51:03 +0700

    Deploy 1.0

--- main.py
+++ main.py
@@ -3,9 +3,17 @@ import uvicorn
 from fastapi import FastAPI
 from modules.login.controller import router as loginRouter
 from modules.teacher.controller import router as TeacherRouter
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
     docs_url='/'
+)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(loginRouter)
